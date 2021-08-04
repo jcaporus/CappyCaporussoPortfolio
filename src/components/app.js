99d61bffc51684a03dc7d72b4f58dd5ef9a73841
jcaporus <jcaporus@amazon.com>
2021-08-03 23:22:01 -0400

    B_02_class_vs_functional_components_react

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import moment from 'moment';
 
+import PortfolioContainer from "./Portfolio/portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>Cappy Caporusso Portfolio</h1>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
+        <PortfolioContainer />
       </div>
     );
   }
