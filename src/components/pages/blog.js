9dde3c8efbab5d7b7f8ce7c9a0c23e27628dd7df
jcaporus <jcaporus@amazon.com>
2021-08-25 08:10:53 -0400

    I_16_refactoring_infinite_scroll_feature_fix_memory_leak_cappy

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -40,7 +40,7 @@ class Blog extends Component {
     });
     axios
       .get(
-        `https://jordan.devcamp.space/portfolio/portfolio_blogs?page=${this
+        `https://cappy.devcamp.space/portfolio/portfolio_blogs?page=${this
           .state.currentPage}`,
         {
           withCredentials: true
